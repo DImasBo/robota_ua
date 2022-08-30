a261c7b714dc75d11e6489021585d3ec7289c1a6
DImasBo <dimasbo.work@gmail.com>
2022-08-30 23:53:43 +0300

    feat: implemented consumer

--- consumer/utils.py
+++ consumer/utils.py
@@ -1,9 +1,19 @@
+from elasticsearch import Elasticsearch
 from kafka import KafkaConsumer
+from datetime import datetime
 import json
 
 
-def send_message_to_elasticsearch(message: dict):
-    pass
+def send_message_to_elasticsearch(index_name, message: dict, elastic_client: Elasticsearch):
+    doc = {
+        'message': message.get('message'),
+        'timestamp': datetime.fromtimestamp(
+            message.get('timestamp')),
+    }
+
+    response = elastic_client.index(index=index_name, body=doc)
+    elastic_client.indices.refresh(index=index_name)
+    return response
 
 
 def get_consumer(*topics: str):
